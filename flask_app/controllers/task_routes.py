73cb215586622de03af2c62d70918d26d6a6c663
Michael <michaeldvoth@me.com>
2023-07-14 18:50:58 -0700

    Finish bootstrap format dates

--- flask_app/controllers/task_routes.py
+++ flask_app/controllers/task_routes.py
@@ -4,6 +4,7 @@ from flask_app.models.users import User
 from flask_app.models.tasks import Task
 from flask_app.models.messages import Message
 
+
 @app.route('/dashboard')
 def dashboard_page():
     if session.get('user_id') is None:
@@ -16,71 +17,76 @@ def dashboard_page():
 
     tasks_with_unread_messages = Task.get_tasks_with_unread_messages(user_id)
 
-    return render_template('dashboard.html', user=user, tasks=tasks, available_tasks=available_tasks, tasks_with_unread_messages=tasks_with_unread_messages)
+    return render_template('dashboard.html', user=user, tasks=tasks, available_tasks=available_tasks,tasks_with_unread_messages=tasks_with_unread_messages)
 
 
-@app.route('/create') # Create task page.
+@app.route('/create')  # Create task page.
 def create_task_page():
     if session.get('user_id') is None:
         return redirect('/')
-    user = User.get_all_users() # Get all users for assignee dropdown.
+    user = User.get_all_users()  # Get all users for assignee dropdown.
     return render_template('create_task.html', user=user)
 
 
-@app.route('/add/task', methods=['POST']) # Add task to DB.
+@app.route('/add/task', methods=['POST'])  # Add task to DB.
 def add_task():
     if session.get('user_id') is None:
         return redirect('/')
-    data = { # Create data dictionary to pass to save method.
-        'title' : request.form['title'],
-        'details' : request.form['details'],
-        'due_date' : request.form['due_date'],
-        'assignee_id' : int(request.form['assignee_id']) if request.form['assignee_id'] != '' else None,
+    data = {  # Create data dictionary to pass to save method.
+        'title': request.form['title'],
+        'details': request.form['details'],
+        'due_date': request.form['due_date'],
+        'assignee_id': int(request.form['assignee_id']) if request.form['assignee_id'] != '' else None,
     }
     Task.save(data)
-    if request.form['assignee_id'] != 'null': # If assignee is not null, save assignee.
-        Task.save_assignee(request.form) # Save assignee to task.
+    # If assignee is not null, save assignee.
+    if request.form['assignee_id'] != 'null':
+        Task.save_assignee(request.form)  # Save assignee to task.
     return redirect('/dashboard')
 
-@app.route('/add/assignee/<int:id>/task/<int:task_id>', methods=['POST']) # Add assignee to task.
+
+# Add assignee to task.
+@app.route('/add/assignee/<int:id>/task/<int:task_id>', methods=['POST'])
 def add_assignee(id, task_id):
     if session.get('user_id') is None:
         return redirect('/')
     data = {
         'id': id,
-        'task_id': task_id 
+        'task_id': task_id
     }
     Task.save_assignee(data)
     return redirect('/dashboard')
 
 
-@app.route('/show/<int:id>') # Show task details page
+@app.route('/show/<int:id>')  # Show task details page
 def show_task_page(id):
     if session.get('user_id') is None:
         return redirect('/')
-    
+
     # Get the task
     task = Task.get_task_by_id({'id': id}, session['user_id'])
 
     # If the current user is the assignee, mark unread messages as read
     if session['user_id'] == task.assignee_id:
         for message in task.unread_messages:
-            Message.set_read({'id': message.id}) 
+            Message.set_read({'id': message.id})
             task.read_messages.append(message)
         task.unread_messages = []
 
     # messages = Message.get_messages({'id': id})
     return render_template('task_details.html', task=task)
 
-@app.route('/edit/<int:id>') # Edit task page
+
+@app.route('/edit/<int:id>')  # Edit task page
 def edit_task(id):
     if session.get('user_id') is None:
         return redirect('/')
     task = Task.get_task_by_id({'id': id}, session['user_id'])
-    user = User.get_all_users() # Get all users for assignee dropdown.
+    user = User.get_all_users()  # Get all users for assignee dropdown.
     return render_template('edit_task.html', task=task, user=user)
 
-@app.route('/update/<int:id>', methods=['POST']) # Update task in DB.
+
+@app.route('/update/<int:id>', methods=['POST'])  # Update task in DB.
 def update_task(id):
     if session.get('user_id') is None:
         return redirect('/')
@@ -94,36 +100,43 @@ def update_task(id):
     Task.update_task(data)
     return redirect('/dashboard')
 
-@app.route('/delete/<int:id>') # Delete task from DB.
+
+@app.route('/delete/<int:id>')  # Delete task from DB.
 def delete_task(id):
     if session.get('user_id') is None:
         return redirect('/')
     Task.delete_task({'id': id})
     return redirect('/dashboard')
 
-@app.route('/complete/<int:id>', methods = ["POST"]) # Mark task as complete.
+
+@app.route('/complete/<int:id>', methods=["POST"])  # Mark task as complete.
 def complete_task(id):
     if session.get('user_id') is None:
         return redirect('/')
     Task.mark_complete({'id': id})
     return redirect('/show/'+str(id))
 
-@app.route('/incomplete/<int:id>', methods = ["POST"]) # Mark task as incomplete.
+
+# Mark task as incomplete.
+@app.route('/incomplete/<int:id>', methods=["POST"])
 def incomplete_task(id):
     if session.get('user_id') is None:
         return redirect('/')
     Task.mark_incomplete({'id': id})
     return redirect('/show/'+str(id))
 
-@app.route('/task/overview') # Task overview page.
+
+@app.route('/task/overview')  # Task overview page.
 def task_overview_page():
     if session.get('user_id') is None:
         return redirect('/')
     tasks = Task.get_all_tasks()
-    user = User.get_by_id({'id': session['user_id']}) # Get user info by id in session.
+    # Get user info by id in session.
+    user = User.get_by_id({'id': session['user_id']})
     return render_template('task_overview.html', tasks=tasks, user=user)
 
-@app.route('/user/<int:id>') # User details page.
+
+@app.route('/user/<int:id>')  # User details page.
 def user_overview_page(id):
     if session.get('user_id') is None:
         return redirect('/')
